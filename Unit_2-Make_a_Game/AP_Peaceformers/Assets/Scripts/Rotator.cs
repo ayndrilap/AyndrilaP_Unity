68a4f0e0cad84a9a3cc64c7d48fe2e9d3bb15919
Ayndrila P. <ayndrilap@nycstudents.net>
2025-01-15 01:20:48 -0500

    yay!

--- Unit_2-Make_a_Game/AP_Peaceformers/Assets/Scripts/Rotator.cs
+++ Unit_2-Make_a_Game/AP_Peaceformers/Assets/Scripts/Rotator.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    public float horizontal;
+    public float speed = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,8 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * Time.deltaTime * speed * horizontal);
+
     }
 }
