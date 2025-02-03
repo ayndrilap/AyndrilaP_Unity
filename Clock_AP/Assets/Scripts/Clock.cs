2c7757cc3947b8cc5aca281247acde23cb594f0c
Ayndrila P. <ayndrilap@nycstudents.net>
2025-02-03 14:23:14 -0500

    YAAAAAAA

--- Clock_AP/Assets/Scripts/Clock.cs
+++ Clock_AP/Assets/Scripts/Clock.cs
@@ -1,3 +1,7 @@
+//Ayndrila Pual
+//period 7-8
+// This code controls the hour, minute and second hand.
+using System;
 using UnityEngine; // this imports preBuild functions for Unity
 
 // a class defines an object and
@@ -11,14 +15,22 @@ public class Clock : MonoBehaviour
 [SerializeField]
 Transform hoursPivot, minutesPivot, secondsPivot;
 
-float hoursToDegrees = -30;
+const float hoursToDegrees = - 30;
+const float minutesToDegrees = -6;
+const float secondsToDegrees = -6;
 // declare a function
-void Awake()
+void Update()
     {
+        //declare a variable for time
+        var time = DateTime.Now;
         //this is only inside the Awake function
         Debug.Log(DateTime.Now);
         hoursPivot.localRotation = 
-            Quaternion.Euler( 0, 0, hoursToDegrees * DateTime.Now.Hour);
+            Quaternion.Euler( 0, 0, hoursToDegrees * time.Hour);
+        minutesPivot.localRotation =
+            Quaternion.Euler( 0, 0, minutesToDegrees * time.Minute);
+        secondsPivot.localRotation =
+             Quaternion.Euler( 0, 0, secondsToDegrees * time.Second);
 
     }
 }
